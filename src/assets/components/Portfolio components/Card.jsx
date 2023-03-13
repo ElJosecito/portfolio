0d23d63d1d1b91ff7b4a7af5737bb129b44fd6d5
ElJosecito <imjose0314@gmail.com>
2023-03-13 19:35:48 -0400

    portfolio route add

--- src/assets/components/Portfolio components/Card.jsx
+++ src/assets/components/Portfolio components/Card.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
 
 function Card() {
   return (
-    <div>Card</div>
-  )
+    <div className="card w-80 sm:w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img
+          src="https://www.dropbox.com/s/pkurybyh28qvp6k/Pokemon%20App.png?raw=1"
+          alt="Pokemon App"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="sm:card-title text-base font-bold">
+          Pokemon App
+          <a href="https://github.com/ElJosecito/Pokemon-simple-app">
+            <div className="badge m-1">Github</div>
+          </a>
+          <a href="https://capable-parfait-86f138.netlify.app">
+            <div className="badge badge-primary">Demo</div>
+          </a>
+        </h2>
+        <p>
+          Esta es una pequeña app usando la PokeApi que te muestra un pokemon
+          random.
+        </p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">HTML</div>
+          <div className="badge badge-outline">CSS</div>
+          <div className="badge badge-outline">JS</div>
+          <div className="badge badge-outline">API's</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
