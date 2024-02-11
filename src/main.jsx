a578164f46ef9d256ee4a088fe7b3c2b76d80b28
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-02-11 00:00:18 -0400

    Update Router import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/assets/styles/style.css";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
