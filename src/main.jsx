ce7ef52baac725124691de3b73def8c0bca429fa
josemartinezflorimon <josemartinezflorimon@gmail.com>
2024-02-11 00:12:09 -0400

    Update homepage URL and use HashRouter instead of Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/assets/styles/style.css";
 import App from "./App";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <Router>
+    <HashRouter>
       <App />
-    </Router>
+    </HashRouter>
   </>
 );
